4057fbbed4fdf194d85016f31c97427edf11c6ad
Alexzeuqram <jared.marquez.is@unipolidgo.edu.mx>
2024-01-20 03:11:55 -0600

    Actualizacion

--- src/components/pages/Articulos.jsx
+++ src/components/pages/Articulos.jsx
@@ -1,47 +1,50 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { Global } from "../../helpers/Global";
+import { Peticion } from "../../helpers/Peticion";
 
 export const Articulos = () => {
   const [articulos, setArticulos] = useState([]);
 
   useEffect(() => {
     conseguirArticulos();
-  }, [])
+  }, []);
 
-  const conseguirArticulos = async() => {
-    const url = "https://jarednodejs.onrender.com/articulos/articulos";
+  const conseguirArticulos = async () => {
     
-    let peticion = await fetch(url, {
-      method: "GET",
-    });
+   const {datos, cargando} = await Peticion(Global.url + "articulos", "GET");
 
-    let datos = await peticion.json();
-    console.log(datos);
-    
-    if(datos.status === "success"){
-     setArticulos(datos.articulos);
+  
+
+    if ((datos.message = "Success")) {
+      
+      setArticulos(datos.data);
     }
   };
 
   return (
     <>
-      {articulos.map((articulo) => {
-        return (
-          <article key={articulos._id} className="articulo-item">
-            <div className="mask">
-              <img src="https://www.nationalgeographic.com.es/medio/2023/08/25/perro_adf93861_1412331876_230825124420_800x800.jpg"></img>
-            </div>
+      {articulos.length >= 1 ? (
+        articulos.map((articulo) => {
+          return (
+            <article key={articulo.id} className="articulo-item">
+              <div className="mask">
+                <img src="https://www.nationalgeographic.com.es/medio/2023/08/25/perro_adf93861_1412331876_230825124420_800x800.jpg"></img>
+              </div>
 
-            <div className="datos">
-              <h3 className="title">{articulos.titulo}</h3>
-              <p className="description">{articulo.contenido}</p>
+              <div className="datos">
+                <h3 className="title">{articulo.titulo}</h3>
+                <p className="description">{articulo.contenido}</p>
 
-              <button className="edit">Editar</button>
-              <button className="delete">Borrar</button>
-            </div>
-          </article>
-        );
-      })}
+                <button className="edit">Editar</button>
+                <button className="delete">Borrar</button>
+              </div>
+            </article>
+          );
+        })
+      ) : (
+        <h1> No hay articulos</h1>
+      )}
     </>
   );
 };
---
+++ src/helpers/Global.jsx
@@ -0,0 +1,7 @@
+
+
+export const Global =  {
+
+    url : "https://jarednodejs.onrender.com/articulos/"
+  
+};
