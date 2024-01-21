604298ad1f5c4e053a7f0d961bee13336b2df50f
Alexzeuqram <jared.marquez.is@unipolidgo.edu.mx>
2024-01-21 13:59:33 -0600

    Update Listado.jsx

--- src/components/pages/Listado.jsx
+++ src/components/pages/Listado.jsx
@@ -10,8 +10,8 @@ export const Listado = ({ articulos, setArticulos }) => {
             return (
                 <article key={articulo.id} className="articulo-item">
                     <div className="mask">
-                        {articulo.imagen  !="default.png"&& <img src={Global.url + "imagen/"+articulo.imagen}></img>}
-                        {!articulo.imagen =="default.png" && <img src="https://www.nationalgeographic.com.es/medio/2023/08/25/perro_adf93861_1412331876_230825124420_800x800.jpg"></img>}
+                        {articulo.imagen != "default.png" && <img src={Global.url + "imagen/" + articulo.imagen}></img>}
+                        {articulo.imagen == "default.png" && <img src="https://www.nationalgeographic.com.es/medio/2023/08/25/perro_adf93861_1412331876_230825124420_800x800.jpg"></img>}
                     </div>
 
                     <div className="datos">
