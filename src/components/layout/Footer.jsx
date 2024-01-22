c501d37ab23979a46497d0a78873e6ca64c87b25
Alexzeuqram <101904746+Alexzeuqram@users.noreply.github.com>
2024-01-22 15:32:51 -0600

    Update.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <footer className="footer">
-      &copy; Master en React -{" "}
+      &copy; React -{" "}
       <a href="https://courageous-toffee-4be3ce.netlify.app/portafolio">Jared Marquez Portafolio React</a>
     </footer>
   );
